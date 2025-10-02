7f72b6f63e23ed7503ec666649da37a89b296966
Alex Smagin <alexsmagin1@gmail.com>
2025-10-02 09:43:56 -0500

    fix: update pages, add privacy policy to pages folder

--- src/routes/RouterConfig.jsx
+++ src/routes/RouterConfig.jsx
@@ -6,11 +6,15 @@ const Home = lazy(() => import('../pages/Home/Home'));
 const Accessibility = lazy(
   () => import('../pages/Accessibility/Accessibility')
 );
+const PrivacyPolicy = lazy(
+  () => import('../pages/PrivacyPolicy/PrivacyPolicyPage')
+);
 
 export const RouterConfig = () =>
   createRoutesFromElements(
     <Route path="/" element={<RootLayout />}>
       <Route index element={<Home />} />
       <Route path="accessibility" element={<Accessibility />} />
+      <Route path="privacy-policy" element={<PrivacyPolicy />} />
     </Route>
   );
