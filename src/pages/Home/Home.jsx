0f166963c1ce889d1d30ad723887d51e9746439a
Soham <solutionnoton@gmail.com>
2025-10-03 12:04:57 +0530

    feat: moved the shopCollection to the Home route

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,8 +1,10 @@
+import CollectionSection from '../ShopCollection/CollectionSection';
 function Home() {
   return (
     <div>
       Hacktoberfest 2025 Page
       <p>Welcome to the Hacktoberfest 2025 Page!</p>
+      <CollectionSection/>
     </div>
   );
 }
--- src/routes/RouterConfig.jsx
+++ src/routes/RouterConfig.jsx
@@ -6,13 +6,11 @@ const Home = lazy(() => import('../pages/Home/Home'));
 const Accessibility = lazy(
   () => import('../pages/Accessibility/Accessibility')
 );
-const CollectionSection = lazy(() => import('../pages/ShopCollection/CollectionSection'));
 
 export const RouterConfig = () =>
   createRoutesFromElements(
     <Route path="/" element={<RootLayout />}>
       <Route index element={<Home />} />
       <Route path="accessibility" element={<Accessibility />} />
-      <Route path="collection" element={<CollectionSection />} />
     </Route>
   );
