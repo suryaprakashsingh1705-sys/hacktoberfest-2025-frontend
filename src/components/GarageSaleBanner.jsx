708bdf58a2fb682c5412b8f8a7b6954e82dc9d5f
Shiv Shukla <shivshukla00514@gmail.com>
2025-10-08 14:14:33 +0530

    fix:convert Garage Sale banner to WebP format

--- src/components/GarageSaleBanner.jsx
+++ src/components/GarageSaleBanner.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
-import garageSaleImage from '../assets/garagesale/garagesale-banner.png'
+import garageSaleImage from '../assets/garagesale/garagesale-banner.webp'
 import './GarageSaleBanner.css'
 
 const GarageSaleBanner = () => {
     return (
-        <div className='garage-sale-banner w-full h-50% mt-24'>
+        <div className='garage-sale-banner w-full mt-24'>
             <img className='w-full h-full object-cover' src={garageSaleImage} alt="Garage Sale promotional banner" loading="lazy" />
         </div>
     )
