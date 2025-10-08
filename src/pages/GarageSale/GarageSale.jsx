86f4252d1f1ed3ca4dc762f03fa8fd38d39052c3
Shiv Shukla <shivshukla00514@gmail.com>
2025-10-08 12:36:50 +0530

    Garage Sale Page – Top Banner added

---
+++ src/pages/GarageSale/GarageSale.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import GarageSaleBanner from '../../components/GarageSaleBanner'
+
+const GarageSale = () => {
+    return (
+        <div className='w-full h-screen overflow-hidden'>
+            <GarageSaleBanner />
+        </div>
+    )
+}
+
+export default GarageSale
