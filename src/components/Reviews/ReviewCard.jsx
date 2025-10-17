7154b049795f398048ff6cd51196b233470bc4b1
sabarish <sabarish_7@protonmail.com>
2025-10-18 03:02:59 +0530

    #26 Reviews section PR, (Duplicate)

---
+++ src/components/Reviews/ReviewCard.jsx
@@ -0,0 +1,25 @@
+import { renderStars } from '../common/ReviewStars';
+
+const ReviewCard = ({ product, comment, name, rating }) => {
+  return (
+    <li className="review-card min-w-[250px] relative max-w-[250px] h-max gap-6 flex flex-col p-6 bg-white border border-[#DDDDDD] items-center justify-center rounded-lg shadow-md cursor-pointer hover:shadow-xl transition-shadow duration-300">
+      <img
+        src={`/icons/material-icon-theme_verified.svg`}
+        alt={'verified'}
+        className="verified-badge absolute top-[-10px] right-[-12px]"
+      />
+      <h3 className="review-title  font-bold text-lg --font-inter text-[#0D1B2A]">
+        {product}
+      </h3>
+      <p className="review-comment text-sm text-center min-h-[calc(1.5em*3)] line-clamp-3  --font-montserrat text-[#333333]">
+        {comment}
+      </p>
+      <h3 className="review-name  font-bold text-lg --font-inter text-[#0D1B2A]">
+        {name}
+      </h3>
+      <div className="flex gap-1">{renderStars(rating)}</div>
+    </li>
+  );
+};
+
+export default ReviewCard;
