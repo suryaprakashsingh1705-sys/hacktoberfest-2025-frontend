1cf0c6cc3c4c3a00e81e049e385711db13f8b3df
pranavtamada <pranav.tamada@gmail.com>
2025-10-21 22:05:34 +0530

    feat: add "Back to Top" button for improved page navigation UX

--- src/layouts/RootLayout.jsx
+++ src/layouts/RootLayout.jsx
@@ -7,6 +7,7 @@ import TopFooter from '../components/TopFooter';
 import MainHeader from '../components/Header';
 import Loader from '../components/Loader';
 import BottomFooter from '../components/BottomFooter';
+import BackToTop from '../components/BackToTop/BackToTop';
 
 function RootLayout() {
   return (
@@ -37,6 +38,7 @@ function RootLayout() {
         draggable
         pauseOnHover
       />
+      <BackToTop showAfter={250} />
     </>
   );
 }
