466533bf9e536c4551c0cd3756c7ccbeeb11e78e
rmbarley <rmbarley@gmail.com>
2025-10-20 22:11:42 -0400

    [117] solve height issues

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -39,13 +39,13 @@ function HeroSection() {
 
   return (
     <section
-      className="hero-section"
+      className="hero-section h-[calc(100vh-4rem)] sm:h-auto xl:h-[calc(100vh-4rem)] overflow-hidden"
       aria-label="Hero carousel"
       id="hero-section"
     >
       <Slider
         ref={sliderRef}
-        className="hero-slider"
+        className="hero-slider h-full"
         autoplay={false}
         speed={1000}
         fade
@@ -54,20 +54,20 @@ function HeroSection() {
         infinite
       >
         {slides.map((slide, index) => (
-          <div key={index}>
-            <picture>
-              <source media="(width < 40rem)" srcset={slide.mobileImage} />
+          <div key={index} className="relative w-full h-full">
+            <picture className="block w-full h-full">
+              <source media="(max-width: 40rem)" srcSet={slide.mobileImage} />
               <img
                 src={slide.desktopImage}
                 alt={slide.alt}
-                className="w-full h-auto block object-cover object-center"
                 tabIndex={-1}
+                className="w-full h-full object-cover object-top sm:object-contain sm:object-center xl:object-cover xl:object-center"
               />
             </picture>
 
             <LinkButton
               href={slide.href}
-              className="absolute bottom-22 right-8 sm:bottom-30 sm:right-21"
+              className="absolute bottom-22 right-8 sm:right-14 sm:bottom-20 md:bottom-30 md:right-21"
             >
               {slide.buttonLabel}
             </LinkButton>
